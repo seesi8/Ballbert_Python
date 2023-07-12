b75decdd73b7cf8d4be562aa93d8884ca60cf593
Samuel Liebert <Samuelliebert@gmail.com>
2023-07-11 22:00:15 -0500

    changed typo

--- Python.py
+++ Python.py
@@ -7,7 +7,7 @@ class Python(Skill):
     @reg(name="Run Python Code")
     def run(self, code):
         """
-        Runs Python code. Returns the result. Uses python eval to run code and get result.
+        Runs Python code. Returns the result. Uses python exec to run code and get result.
 
         :param string code: The code you want to run.
         :return: The Result.
@@ -15,7 +15,7 @@ class Python(Skill):
         """
 
         try:
-            res = eval(code)
+            res = exec(code)
             return Response(succeeded=False, data=res)
         except Exception as e:
             return Response(succeeded=False, data=f"error: {e}")
